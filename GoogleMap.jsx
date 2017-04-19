bcfe00a933a118e2b0903fccb5ad3e469890ace2
Christopher Ganey <christophganey@gmail.com>
2017-04-18 22:46:59 -0500

    Create GoogleMap.jsx

---
+++ GoogleMap.jsx
@@ -0,0 +1,42 @@
+ "use strict";
+
+var React = require('react');
+
+module.exports = React.createClass({
+	
+	getInitialState: function() {
+		return {
+			
+		};
+	},
+	getLocale: function(locale) {
+		locale = locale.trim().split(' ').join('+');
+			this.setState({ locale: locale });
+	},
+	componentDidMount: function() {
+		if(typeof window !== 'undefined'){
+			var locale = this.props.eventLocale;
+			this.getLocale(locale);
+		}
+	},
+	componentWillReceiveProps: function(nextProps) {
+		if(this.props.eventLocale !== nextProps.eventLocale) {
+			var locale = nextProps.eventLocale;
+			this.getLocale(locale);
+		}	
+	  }, 
+	
+  render() {
+
+    return (
+      <iframe
+		  width="600"
+		  height="450"
+		  frameBorder="0" 
+		  src={"https://www.google.com/maps/embed/v1/place?key=YOURVERYLONGKEY=" + this.state.locale + "&zoom=17"} allowFullScreen>
+		</iframe>
+    );
+  }
+
+});
+
